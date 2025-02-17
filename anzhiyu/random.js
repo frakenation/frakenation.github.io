6d89096683cd4c1d8c2294ab4363541462a59d98
frakenation <2638456208@qq.com>
2025-02-17 19:11:30 +0800

    Site updated: 2025-02-17 19:11:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/03/第二篇文章/","2025/02/03/hello-world/"];function toRandomPost(){
+var posts=["2025/02/03/hello-world/","2025/02/03/第二篇文章/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
